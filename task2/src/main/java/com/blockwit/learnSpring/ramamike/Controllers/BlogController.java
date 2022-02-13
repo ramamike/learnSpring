e1488d52672bdb8c6ece23874701c16a48487e05
ramamike <7629756@gmail.com>
2022-02-13 06:53:29 +0300

    src/main is added

--- task2/src/main/java/com/blockwit/learnSpring/ramamike/Controllers/BlogController.java
+++ task2/src/main/java/com/blockwit/learnSpring/ramamike/Controllers/BlogController.java
@@ -1,4 +1,13 @@
 package com.blockwit.learnSpring.ramamike.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BlogController {
+    @GetMapping("/blog")
+    public String blogMain(Model model) {
+        return "blog-main";
+    }
 }
