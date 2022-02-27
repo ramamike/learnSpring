e77383d1f678c22ee25c9e29ce0cc0ca3b9517ab
ramamike <7629756@gmail.com>
2022-02-27 06:06:03 +0300

    blog is ready

--- task2/src/main/java/com/blockwit/learnSpring/ramamike/Controllers/BlogController.java
+++ task2/src/main/java/com/blockwit/learnSpring/ramamike/Controllers/BlogController.java
@@ -60,4 +60,20 @@ public class BlogController {
         model.addAttribute("post", res);
         return "blog-edit";
     }
+
+    @PostMapping("/blog/{id}/Edit")
+    public String blogPostUpdate(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String anons,@RequestParam String full_text, Model model) {
+        Post post=postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+    @PostMapping("/blog/{id}/Remove")
+    public String blogPostUpdate(@PathVariable(value = "id") long id, Model model) {
+        Post post=postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+        return "redirect:/blog";
+    }
 }
