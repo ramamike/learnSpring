a0830ce789ee4e280530942ac2df50ae82c1146c
ramamike <7629756@gmail.com>
2022-04-14 06:57:02 +0300

    currency table on the home.html

--- curerency/src/main/java/com/learn/spring/curerency/contoller/MainController.java
+++ curerency/src/main/java/com/learn/spring/curerency/contoller/MainController.java
@@ -1,15 +1,22 @@
 package com.learn.spring.curerency.contoller;
 
+import com.learn.spring.curerency.service.CurrencyParsing;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(Model model) throws IOException {
+        CurrencyParsing currencyParsing = new CurrencyParsing();
+        List<List<String>> currencies = currencyParsing.getCurrencies();
         model.addAttribute("title", "Главная страница");
+        model.addAttribute("currencies", currencies);
         return "home";
     }
 
