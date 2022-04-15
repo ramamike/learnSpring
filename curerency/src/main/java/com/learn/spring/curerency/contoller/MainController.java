fdac4dbfb05586a6a6de801c9f0b315a9813a1f2
ramamike <7629756@gmail.com>
2022-04-15 06:32:00 +0300

    Currency parsing with exceptions and scheduling is ready

--- curerency/src/main/java/com/learn/spring/curerency/contoller/MainController.java
+++ curerency/src/main/java/com/learn/spring/curerency/contoller/MainController.java
@@ -1,22 +1,32 @@
 package com.learn.spring.curerency.contoller;
 
 import com.learn.spring.curerency.service.CurrencyParsing;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.io.IOException;
 import java.util.List;
 
 @Controller
 public class MainController {
 
+
+    @Autowired
+    public MainController(CurrencyParsing currencyParsing) {
+        this.currencyParsing = currencyParsing;
+    }
+
+    private CurrencyParsing currencyParsing;
+    private long counterOfUpdating =0L;
+
+
     @GetMapping("/")
-    public String home(Model model) throws IOException {
-        CurrencyParsing currencyParsing = new CurrencyParsing();
+    public String home(Model model) {
         List<List<String>> currencies = currencyParsing.getCurrencies();
         model.addAttribute("title", "Главная страница");
         model.addAttribute("currencies", currencies);
+        model.addAttribute("currentTime", counterOfUpdating++);
         return "home";
     }
 
--- curerency/src/main/java/com/learn/spring/curerency/service/CurrencyParsing.java
+++ curerency/src/main/java/com/learn/spring/curerency/service/CurrencyParsing.java
@@ -4,6 +4,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -12,9 +14,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@EnableScheduling
 public class CurrencyParsing {
 
-    //private final String urlCurrency="http://www.cbr.ru/";
+    //private final String urlCurrency="http://www.cbr.ruu/";
     private final String urlCurrency="https://belarusbank.by/be";
 
     public Document getPage() throws IOException{
@@ -22,10 +25,27 @@ public class CurrencyParsing {
         return page;
     }
 
-    public List<List<String>> getCurrencies() throws IOException {
-        Document page=Jsoup.parse(new URL(urlCurrency), 5000);
+    @Scheduled(fixedRate = 5000)
+    public List<List<String>> getCurrencies(){
+
+        Document page= null;
+        try {
+            page = Jsoup.parse(new URL(urlCurrency), 5000);
+        } catch (IOException e) {
+            System.out.println(e.getMessage() + " is not available");
+            return null;
+        }
+
+        if(page==null) {
+            return null;
+        }
+
         Element currencyTable=page.select("table[class=currency-table currency-table--row-sm]")
                 .first();
+
+        if(currencyTable==null) {
+            return null;
+        }
 
         Elements currencyRows=currencyTable.select("tr");
         List<List<String>> currencies= new ArrayList<>();
@@ -40,7 +60,6 @@ public class CurrencyParsing {
             }
             currencies.add(currencyTableEntries);
         }
-
         return currencies;
     }
 }
