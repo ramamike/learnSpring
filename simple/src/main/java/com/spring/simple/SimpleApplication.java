2d4036ff21b1261e6bc85dd686db2e37696329ff
ramamike <7629756@gmail.com>
2022-04-18 19:54:32 +0300

    -----slf4j----

--- simple/src/main/java/com/spring/simple/SimpleApplication.java
+++ simple/src/main/java/com/spring/simple/SimpleApplication.java
@@ -1,13 +1,19 @@
 package com.spring.simple;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimpleApplication {
 
+	static Logger logger = LoggerFactory.getLogger(SimpleApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApplication.class, args);
+
+		logger.info("main is started");
+
 	}
 
 }
