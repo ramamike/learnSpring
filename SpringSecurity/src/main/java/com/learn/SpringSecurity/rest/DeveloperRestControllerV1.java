11ec14209acf78331dfe55a0bd8c7e030319a771
ramamike <7629756@gmail.com>
2022-05-18 13:35:23 +0300

    getAll and getById

--- SpringSecurity/src/main/java/com/learn/SpringSecurity/rest/DeveloperRestControllerV1.java
+++ SpringSecurity/src/main/java/com/learn/SpringSecurity/rest/DeveloperRestControllerV1.java
@@ -1,10 +1,33 @@
 package com.learn.SpringSecurity.rest;
 
+import com.learn.SpringSecurity.model.Devoloper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("api/v1/developers")
 public class DeveloperRestControllerV1 {
 
+    private List<Devoloper> developers = Stream.of(
+            new Devoloper(1l, "ivan", "ivanou"),
+            new Devoloper(2l, "petr", "petrou"),
+            new Devoloper(3l, "sidar", "sidarau")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Devoloper> getAll(){
+        return developers;
+    }
+
+    @GetMapping("/{id}")
+    public Devoloper getById(@PathVariable("id") long id){
+        return developers.stream().filter(devoloper -> devoloper.getId().equals(id))
+                .findFirst().orElse(null);
+    }
 }
