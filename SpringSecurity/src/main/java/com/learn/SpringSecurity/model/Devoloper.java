74b311ca9f66bdd9ef1bb41aafc643cac0d1e5c5
ramamike <7629756@gmail.com>
2022-05-18 12:09:05 +0300

    Spring security learning, start

---
+++ SpringSecurity/src/main/java/com/learn/SpringSecurity/model/Devoloper.java
@@ -0,0 +1,12 @@
+package com.learn.SpringSecurity.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Devoloper {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
---
+++ SpringSecurity/src/main/java/com/learn/SpringSecurity/rest/DeveloperRestControllerV1.java
@@ -0,0 +1,10 @@
+package com.learn.SpringSecurity.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/developers")
+public class DeveloperRestControllerV1 {
+
+}
