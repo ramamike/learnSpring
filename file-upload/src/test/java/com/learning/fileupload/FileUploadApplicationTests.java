c78a3435662d9c289bcf63935703aa917ad1994e
ramamike <7629756@gmail.com>
2022-07-02 09:02:32 +0300

    upload file learning

---
+++ file-upload/src/test/java/com/learning/fileupload/FileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.fileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
